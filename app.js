b5f26e3878c582def661b8bdedaef540a94132cf
ekuhnle <epk800@gmail.com>
2023-02-27 14:24:16 -0500

    build 1.1

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
+const volleyball = require("volleyball");
+const postBank = require("./postBank");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(volleyball);
+
+app.get("/", (req, res)=>{
+  const posts = postBank.list();
+  const mappedPost = posts.map(post => {
+    return(
+      `<li>Author: ${post.name} Title: ${post.title}</li>`
+    )
+  })
+  const html = `
+  <html>
+    <head>
+      <title>Wizard News</title>
+    </head>
+    <body>
+      <ul>
+        ${mappedPost.join('')}
+      </ul>
+    </body>
+  </html>`;
+  res.send(html)
+})
 
 const PORT = 1337;
 
