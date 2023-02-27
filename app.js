222d6d099a802c7b39b67b415af1da3cd2e1d99d
ekuhnle <epk800@gmail.com>
2023-02-27 14:34:17 -0500

    Build 1.2

--- app.js
+++ app.js
@@ -5,25 +5,36 @@ const postBank = require("./postBank");
 
 
 app.use(volleyball);
+app.use(express.static("public"));
+
 
 app.get("/", (req, res)=>{
   const posts = postBank.list();
-  const mappedPost = posts.map(post => {
-    return(
-      `<li>Author: ${post.name} Title: ${post.title}</li>`
-    )
-  })
-  const html = `
+  
+  const html = `<!DOCTYPE html>
   <html>
-    <head>
-      <title>Wizard News</title>
-    </head>
-    <body>
-      <ul>
-        ${mappedPost.join('')}
-      </ul>
-    </body>
-  </html>`;
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`;
   res.send(html)
 })
 
